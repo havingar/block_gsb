32b9993a731a3113d6cf701326f1e80a2e7a9fa2
Richard Havinga <r.havinga@ulcc.ac.uk>
2013-07-15 13:00:38 +0100

    Incorporate Additional Table Columns

--- db/upgrade.php
+++ db/upgrade.php
@@ -1,18 +1,60 @@
 <?php
 
 
-function xmldb_block_gsb_upgrade($oldversion = 0) {
+function xmldb_block_gsb_upgrade($oldversion) {
     global $DB;
 	$dbman = $DB->get_manager();
 
     return true;
 	
-	if($result && $oldversion <= '2012103000') {
+	if($oldversion <= '2012103000') {
 
 		if($dbman->table_exists('block_gsb_content')){
 			$dbman->rename_table('block_gsb_content', 'block_gsb');
 		}
 	}
+	
+	if($oldversion <= '2013061500') {
+	
+	    $table = 'block_gsb';
+        $xmlfield	=	new $xmldb_field('labelsnum');
+        if (!$dbman->field_exists($table,$xmlfield)) {
+       		
+       		$xmlfield->$set_attributes(XMLDB_TYPE_int,'6',XMLDB_UNSIGNED, XMLDB_NOTNULL);
+       		$dbman->add_field($table,$xmlfield);
+        }
+
+        $xmlfield	=	new $xmldb_field('labelsnum');
+        if (!$dbman->field_exists($table,$xmlfield)) {
+       		
+       		$xmlfield->$set_attributes(XMLDB_TYPE_int,'6',XMLDB_UNSIGNED, XMLDB_NOTNULL);
+       		$dbman->add_field($table,$xmlfield);
+        }        
+
+        $xmlfield	=	new $xmldb_field('foldersnum');
+        if (!$dbman->field_exists($table,$xmlfield)) {
+       		
+       		$xmlfield->$set_attributes(XMLDB_TYPE_int,'6',XMLDB_UNSIGNED, XMLDB_NOTNULL);
+       		$dbman->add_field($table,$xmlfield);
+        }
+
+        $xmlfield	=	new $xmldb_field('headingsnum');
+        if (!$dbman->field_exists($table,$xmlfield)) {
+       		
+       		$xmlfield->$set_attributes(XMLDB_TYPE_int,'6',XMLDB_UNSIGNED, XMLDB_NOTNULL);
+       		$dbman->add_field($table,$xmlfield);
+        }
+
+        $xmlfield	=	new $xmldb_field('urlsnum');
+        if (!$dbman->field_exists($table,$xmlfield)) {
+       		
+       		$xmlfield->$set_attributes(XMLDB_TYPE_int,'6',XMLDB_UNSIGNED, XMLDB_NOTNULL);
+       		$dbman->add_field($table,$xmlfield);
+        }
+		
+	}
+	
+	
 }
 
 ?>
